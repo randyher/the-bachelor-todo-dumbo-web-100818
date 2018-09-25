9a4802e98305f918a13c555c74371f9554ccbd02
Randy Herasme <randyherasme6@gmail.com>
2018-09-25 04:26:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,12 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  full_name=""
-  data.to_sym[season.to_sym].each do |key|
-    if (key[:status]=="Winner")
-      full_name=key[:name]
-    end
-  end
-  output=full_name.split(" ")
-  output[0]
+  return data
 end
 
 def get_contestant_name(data, occupation)
